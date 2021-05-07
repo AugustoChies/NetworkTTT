d1514fa3890c0231c7d717661b4e42bfd042efa6
AugustoChies <guto_chies@hotmail.com>
2021-05-07 01:33:34 -0300

    Added prefab folder. Done basic connection menu and first build

--- Assets/Scripts/GameUIController.cs
+++ Assets/Scripts/GameUIController.cs
@@ -4,27 +4,61 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using MLAPI;
+using MLAPI.Transports.UNET;
 
 public class GameUIController : MonoBehaviour
 {   
+    [Header("Game End Properties")]
+    public GameObject gameCanvas;
     public TextMeshProUGUI winnerText;
+
+    [Space()]
+    [Header("Main Menu Properties")]
+    public GameObject menuCanvas;
+    public TMP_InputField adressField;
+
+    [Space()]
+    public GameObject waitCanvas;
     public Board board;
-    // Start is called before the first frame update
-    void Start()
+
+    private readonly string localhost = "127.0.0.1";
+    
+    
+
+    public void StartServerButton()
     {
-        this.GetComponent<Canvas>().enabled = false;
+        waitCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+        NetworkManager.Singleton.StartHost();
     }
+
+    public void StartClientButton()
+    {
+        NetworkManager manager = NetworkManager.Singleton;
+        if (adressField.text == "")
+        {
+            manager.GetComponent<UNetTransport>().ConnectAddress = localhost;
+        }
+        else
+        {
+            manager.GetComponent<UNetTransport>().ConnectAddress = adressField.text;
+        }
+        manager.StartClient();
+    }
+
 
     public void GameEnd(int value)
     {
-        this.GetComponent<Canvas>().enabled = true;
+        gameCanvas.SetActive(true);        
+       
         switch (value)
         {
-            case 1:                
+            case 1:
                 winnerText.text = "TicTacs won!!!";
                 winnerText.color = Color.green;
                 break;
-            case 2:                
+            case 2:
                 winnerText.text = "Toes won!!!";
                 winnerText.color = Color.red;
                 break;
@@ -37,17 +71,19 @@ public class GameUIController : MonoBehaviour
         }
     }
 
+
     public void RetryButton()
     {
         this.GetComponent<AudioSource>().Play();
-        this.GetComponent<Canvas>().enabled = false;
-        board.Setup();        
+        gameCanvas.SetActive(false);
+        board.Setup();
     }
 
     public void MenuButton()
     {
         this.GetComponent<AudioSource>().Play();
-        SceneManager.LoadScene("MenuScene");
+        menuCanvas.SetActive(true);
+        gameCanvas.SetActive(false);
     }
 
 }
